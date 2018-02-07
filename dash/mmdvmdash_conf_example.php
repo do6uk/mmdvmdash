9dbe52bf03a0cdb90fca189a7fc935545bb26638
do6uk <33249819+do6uk@users.noreply.github.com>
2018-02-07 19:54:23 +0100

    updated for mysql-support
    added lines for mysql-support

--- dash/mmdvmdash_conf_example.php
+++ dash/mmdvmdash_conf_example.php
@@ -1,4 +1,20 @@
 <?php
+	# full path of sqlite-database
 	$SQLite_Path = '/opt/mmdvmdash/mmdvm.db';
+	
+	# host/ip of mysql-server
+	$MySQL_Host = 'localhost';
+	# port of mysql-server
+	$MySQL_Port = '3306';
+	# name of database
+	$MySQL_DB = 'dash';
+	# username for mysql
+	$MySQL_User = 'dash';
+	# password for mysql
+	$MySQL_Pass = 'Secret';
+
+	# define database-connection
+	# mysql | sqlite
+	$Use_DB = 'mysql';
 ?>
 
